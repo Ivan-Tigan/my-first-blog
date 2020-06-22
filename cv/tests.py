85f1faa10dd11f6a9cc09412fb978fda4e2fcf2b
Ivan Tsoninski <vankata@pop-os.localdomain>
2020-06-22 09:51:36 +0100

    some tests and only authenticated can edit CV

--- cv/tests.py
+++ cv/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from django.urls import resolve
+from django.http import HttpRequest
+
+from .views import cv, cv_edit
 
 # Create your tests here.
+class CVTest(TestCase):
+    def test_cv_url_resolves_to_cv_view(self):
+        found = resolve("/cv/")
+        self.assertEqual(found.func, cv)
+    def test_cv_returns_correct_template(self):
+        response = self.client.get('/cv/')
+        html = response.content.decode('utf8')
+
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>CV</title>', html)
+        self.assertTrue(html.endswith('</html>'))
+
+        self.assertTemplateUsed(response, 'cv.html')
+        
+    def test_cv_edit_url_resolved_to_cv_edit_view(self):
+        self.assertEqual(resolve("/cv/edit/").func, cv_edit)
